46b813b0f25baf29d4be96b329a8df49b9e67466
andrei.fomin <andrei.fomin@forrent.com>
2016-06-22 11:33:01 -0400

    Refactor site text using Locale. COR-4052

--- src/main/java/com/forrent/beans/propertyListing/SiteText.java
+++ src/main/java/com/forrent/beans/propertyListing/SiteText.java
@@ -1,39 +1,14 @@
 package com.forrent.beans.propertyListing;
 
 import java.io.Serializable;
+import java.util.Map;
 
 @SuppressWarnings("serial")
 public class SiteText implements Serializable
 {
     private String id;
 
-    private Text text;
-
-    private class Text
-    {
-        private String en;
-        private String es;
-
-        public String getEn()
-        {
-            return en;
-        }
-
-        public void setEn(String en)
-        {
-            this.en = en;
-        }
-
-        public String getEs()
-        {
-            return es;
-        }
-
-        public void setEs(String es)
-        {
-            this.es = es;
-        }
-    }
+    private Map<Locale, String> text;
 
     public String getId()
     {
@@ -45,12 +20,12 @@ public class SiteText implements Serializable
         this.id = id;
     }
 
-    public Text getText()
+    public Map<Locale, String> getText()
     {
         return text;
     }
 
-    public void setText(Text text)
+    public void setText(Map<Locale, String> text)
     {
         this.text = text;
     }
