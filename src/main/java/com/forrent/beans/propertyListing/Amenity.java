e2ffc2a8cd9d15791c100ccafde808c123ded95f
kalmas <kyle.almas@dominionenterprises.com>
2016-06-24 09:00:34 -0400

    change fp summary to list

--- src/main/java/com/forrent/beans/propertyListing/Amenity.java
+++ src/main/java/com/forrent/beans/propertyListing/Amenity.java
@@ -3,50 +3,51 @@ package com.forrent.beans.propertyListing;
 import java.io.Serializable;
 
 @SuppressWarnings("serial")
-public class Amenity implements Serializable {
-	private String category;
-	private String searchableCategory;
-	private String websiteCode;
-	private Boolean searchable;
-	private String name;
-	
-	public String getCategory() {
-		return category;
-	}
-	
-	public void setCategory(final String category) {
-		this.category = category;
-	}
-	
-	public String getSearchableCategory() {
-		return searchableCategory;
-	}
-	
-	public void setSearchableCategory(final String searchableCategory) {
-		this.searchableCategory = searchableCategory;
-	}
-	
-	public String getWebsiteCode() {
-		return websiteCode;
-	}
-	
-	public void setWebsiteCode(final String websiteCode) {
-		this.websiteCode = websiteCode;
-	}
-	
-	public Boolean getSearchable() {
-		return searchable;
-	}
-	
-	public void setSearchable(final Boolean searchable) {
-		this.searchable = searchable;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(final String name) {
-		this.name = name;
-	}
+public class Amenity implements Serializable
+{
+    private String category;
+    private String name;
+    private Boolean searchable;
+    private String searchableCategory;
+    private String websiteCode;
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Boolean getSearchable() {
+        return this.searchable;
+    }
+
+    public String getSearchableCategory() {
+        return this.searchableCategory;
+    }
+
+    public String getWebsiteCode() {
+        return this.websiteCode;
+    }
+
+    public void setCategory(final String category) {
+        this.category = category;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public void setSearchable(final Boolean searchable) {
+        this.searchable = searchable;
+    }
+
+    public void setSearchableCategory(final String searchableCategory) {
+        this.searchableCategory = searchableCategory;
+    }
+
+    public void setWebsiteCode(final String websiteCode) {
+        this.websiteCode = websiteCode;
+    }
 }
