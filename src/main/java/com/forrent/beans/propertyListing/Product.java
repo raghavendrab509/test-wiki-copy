42a35f2d1dea19ce32af4c8551920fdf0574bb26
Silver Ibenye <silver.ibenye@forrent.com>
2017-03-20 14:12:40 -0400

    Delete product class

--- src/main/java/com/forrent/beans/propertyListing/Product.java
+++
@@ -1,9 +0,0 @@
-package com.forrent.beans.propertyListing;
-
-public class Product
-{
-    public static final String TYPE_COMMUNITY_THEATER = "CommThtr";
-    public static final String TYPE_AERIAL_VIDEO = "AerialVideo";
-    public static final String TYPE_SPANISH_LANGUAGE = "LangES";
-
-}
