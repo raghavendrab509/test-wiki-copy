939ad9fc69e8ce26af49c462704219ca1aee2779
Silver Ibenye <silver.ibenye@forrent.com>
2017-03-23 11:08:15 -0400

    Add DerivedProduct class

---
+++ src/main/java/com/forrent/beans/propertyListing/DerivedProduct.java
@@ -0,0 +1,7 @@
+package com.forrent.beans.propertyListing;
+
+public class DerivedProduct
+{
+    public static final String E_MARKETING_EXCEPTION = "eMarketingException_derived";
+    public static final String CONTACT_SUGGESTABLE = "contactSuggestable_derived";
+}
