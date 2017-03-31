0428d9e3f57489aab81d5c160e1c38c18cf1c22f
andrei.fomin <andrei.fomin@forrent.com>
2017-03-31 16:49:06 -0400

    Add FRU_Paid_derived product

--- src/main/java/com/forrent/beans/propertyListing/DerivedProduct.java
+++ src/main/java/com/forrent/beans/propertyListing/DerivedProduct.java
@@ -4,4 +4,5 @@ public class DerivedProduct
 {
     public static final String E_MARKETING_EXCEPTION = "eMarketingException_derived";
     public static final String CONTACT_SUGGESTABLE = "contactSuggestable_derived";
+    public static final String FRU_PAID = "FRU_Paid_derived";
 }
